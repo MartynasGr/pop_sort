379289c5d3ef623e4d2319d801d4781136f7b20e
MartynasGre <martynas.greicius@ecowood.eu>
2024-04-20 12:01:12 +0300

    [FEATURE] Add bubble sort implementation and tests
    A new file has been created to house the bubble sort algorithm. This algorithm takes in an array and sorts it. Additional tests were also added to the test_sort file to verify the correctness of this new algorithm. The test_sort file now also uses a pytest.fixture to setup arrays for testing.

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,7 +1,25 @@
-from pop_sort.selection_sort  import selection_sort
+import pytest
+from pop_sort.selection_sort import selection_sort
+from pop_sort.bubble_sort import bubble_sort
+
+sorted_array_1 = [1, 1, 4, 5, 5, 8, 10, 15, 22, 111]
+sorted_array_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-def test_selection_sort():
-    original_array = [5, 8, 4, 15, 111, 1]
-    sorted_array = [1, 4, 5, 8, 15, 111]
-    assert selection_sort(original_array) == sorted_array
+@pytest.fixture
+def array_fixture():
+    original_array_1 = [5, 8, 4, 15, 111, 1, 22, 1, 5, 10]
+    original_array_2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    return original_array_1.copy(), original_array_2.copy()
+
+
+def test_selection_sort(array_fixture):
+    array_1, array_2 = array_fixture
+    assert selection_sort(array_1) == sorted_array_1
+    assert selection_sort(array_2) == sorted_array_2
+
+
+def test_bubble_sort(array_fixture):
+    array_1, array_2 = array_fixture
+    assert bubble_sort(array_1) == sorted_array_1
+    assert bubble_sort(array_2) == sorted_array_2
